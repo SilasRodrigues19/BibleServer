46f568b8527834b7f1ad3c17889bdbeb4eded55c
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2024-01-09 14:01:48 -0300

    chore: setup server, prisma and build config

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { fastifyCors } from '@fastify/cors';
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: '*',
+});
+
+
+const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
+
+app
+  .listen({
+    port: 3333,
+    host,
+  })
+  .then(() => console.log('Server is running!'))
+  .catch((err) => console.error(err));
